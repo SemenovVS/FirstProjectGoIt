3057203b97d500bff26df05de383c1d1588df09c
Semenov Volodymyr <thesemenovv@gmail.com>
2023-04-12 16:11:34 +0300

    Initial commit

---
+++ src/main/java/com/example/firstprojectgoit/FirstProjectGoItApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstprojectgoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectGoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectGoItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstprojectgoit/FirstProjectGoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstprojectgoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectGoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
